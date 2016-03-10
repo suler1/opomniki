791104cedc53e20b220fe8c1eb2e7112289eeb92
David Šuler <david.kozar1@gmail.com>
2016-03-10 09:39:38 +0000

    dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -11,16 +11,15 @@ window.addEventListener('load', function() {
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 	
 	var dodajOpomnik = function(event) {
-		var naziv_opomnika = document.querySelector("naziv_opomnika").value;
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
 		var cas_opomnika = document.querySelector("#cas_opomnika").value;
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		document.querySelector("#opomniki").innerHTML += " \
-		<div class = 'opomnik'>
-			<div class = 'naziv_opomnika'> + naziv_opomnika + "</div>
-			<div class = 'cas_opomnika'> + Opomnik čez <span>" + cas_opomnika + 
-				"</span> sekund. </div> \
-		</div>";
+		<div class = 'opomnik'> \
+			<div class = 'naziv_opomnika'>" + naziv_opomnika + "</div> \
+			<div class = 'cas_opomnika'>  Opomnik čez  <span>" + cas_opomnika + "</span> sekund. </div> \
+		</div>"
 	}
 	document.querySelector("dodajGumb").addEventListener('click', dodajOpomnik);
 	
@@ -36,6 +35,15 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			
+			if (cas == 0) {
+				var naziv_opomnika = opomnik.querySelector (".naziv_opomnika").innerHTML;
+				alert ("Opomnik!\n\nZadolžitev'" + naziv_opomnika + "' je potekla!");
+				document.querySelector("opomniki").removeChild(opomnik);
+			}
+			else {
+				casovnik.innerHTML = cas - 1;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
